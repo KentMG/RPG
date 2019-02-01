3b08cc9ff8538cd877741f91d59e228debf2bf31
KentMG <4gunns.k@gmail.com>
2019-02-01 14:12:12 -0600

    Added mapSize

--- Movement.js
+++ Movement.js
@@ -1,6 +1,13 @@
-function movePlayer(direction){
-    if(direction=="LEFT"){
-        Player1.X-=1;
-        let leftline=
+import { BrowserWindowProxy } from "electron";
+
+function movePlayer(e){
+    let map=document.getElementById('Board');
+    if(e.key == 'w'){
+        Player1.y -=1;
+        for(var i = player1.x -10; i<player1.x +10; i++){
+            if(i>=0){
+                map[i][player1.y-10].style.display = '';
+            }
+        }
     }
 }
